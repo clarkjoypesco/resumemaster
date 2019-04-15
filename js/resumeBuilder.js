d6b7b0941ee1c3819946be249802a3058a27b54d
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-15 17:45:09 +0800

    added solution

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -64,35 +64,53 @@ $("#main").append(skills[0]);
 
 var sampleArray = [0,0,7];
 
-var incrementLastArrayElement = function(_array) {
-    var newArray = [];
-    // Your code should make newArray equal to an array that has the same
-    // values as _array, but the last number has increased by one.
+// var incrementLastArrayElement = function(_array) {
+//     var newArray = [];
+//     // Your code should make newArray equal to an array that has the same
+//     // values as _array, but the last number has increased by one.
     
-    // For example:
-    // _array = [1, 2, 3];
-    // turns into:
-    // newArray = [1, 2, 4];
+//     // For example:
+//     // _array = [1, 2, 3];
+//     // turns into:
+//     // newArray = [1, 2, 4];
     
-    // Your code goes in here!
-    var index = 0;
+//     // Your code goes in here!
+//     var index = 0;
   
-    while(index < _array.length){
+//     while(index < _array.length){
        
         
-        if (index == _array.length - 1){
-            newArray[index] = _array[index] + 1;
-        } else {
-            newArray[index] = _array[index]
-        }
-        index++
-    }
+//         if (index == _array.length - 1){
+//             newArray[index] = _array[index] + 1;
+//         } else {
+//             newArray[index] = _array[index];
+//         }
+//         index++;
+//     }
 
     
     
-    // Don't delete this line!
+//     // Don't delete this line!
+//     return newArray;
+// };
+function incrementLastArrayElement(_array)  {
+    var newArray = [];
+    newArray = _array.slice(0);
+    var lastNumber = newArray.pop();
+    newArray.push(lastNumber + 1);
     return newArray;
-};
+}
+
+
+
 
 // Did your code work? The line below will tell you!
 console.log(incrementLastArrayElement(sampleArray));
+
+
+
+
+
+
+
+
