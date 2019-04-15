eca338bf32705400c34e8f6fd84ef8157e29b5bb
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-15 18:23:02 +0800

    few changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -113,7 +113,7 @@ var name = "AlbERt EINstEiN";
 
 function nameChanger(oldName) {
     var finalName = oldName;
-    var names = oldName.split(" ");
+    var names = finalName.split(" ");
     names[1] = names[1].toUpperCase();
     names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
     finalName = names.join(" ");
