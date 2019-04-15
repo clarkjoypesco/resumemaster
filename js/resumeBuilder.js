f37d012a7723f340dce985acb09d8a29edf1231b
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-15 13:52:04 +0800

    practice js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+$("#main").append("Clark Joy Pesco")
+
+var firstName = 'Clark Joy';
+
+var age = 25;
+
+console.log('Name: ' +firstName + '\nAge: '+ age);
